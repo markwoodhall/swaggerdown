9084aea2e84ab362f8beebd57a5f352507ce51cb
Mark Woodhall <mark.woodhall@gmail.com>
2017-12-10 07:16:23 +0000

    Add support for reading yaml specs

--- src/swaggerdown/swagger.clj
+++ src/swaggerdown/swagger.clj
@@ -1,10 +1,22 @@
 (ns swaggerdown.swagger
   (:require [yada.yada :refer [listener resource as-resource]]
+            [yaml.core :as y]
             [cheshire.core :refer [parse-string]]))
+
+(defn json?
+  [raw-string]
+  (and (clojure.string/starts-with? raw-string "{")
+       (clojure.string/ends-with? raw-string "}")))
+
+(defn yaml-or-json
+  [raw-string keywords?]
+  (if (json? raw-string)
+    (parse-string raw-string keywords?)
+    (y/parse-string raw-string)))
 
 (defn swagger
   [url keywords?]
   (let [raw-response (slurp url)
-        parsed (parse-string raw-response keywords?)]
+        parsed (yaml-or-json raw-response keywords?)]
     parsed))
 
