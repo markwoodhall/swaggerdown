69a24944665de06d85714eac9706d4631ac00751
Mark Woodhall <mark.woodhall@gmail.com>
2017-12-11 17:02:24 +0000

    Fix issue with int keys and ordered maps for yaml parsing

--- src/swaggerdown/swagger.clj
+++ src/swaggerdown/swagger.clj
@@ -1,7 +1,20 @@
 (ns swaggerdown.swagger
-  (:require [yada.yada :refer [listener resource as-resource]]
+  (:require [clojure.walk :refer [postwalk]]
+            [yada.yada :refer [listener resource as-resource]]
             [yaml.core :as y]
             [cheshire.core :refer [parse-string]]))
+
+(defn disorder [ordering-map map-fn] 
+  (postwalk #(if (map? %) (into map-fn %) %) ordering-map))
+
+(defn keywordize-keys
+  [m]
+  (let [f (fn [[k v]] 
+            (let [sk (if (number? k) (str k) k)]
+              (if (string? sk) 
+                [(keyword sk) v] 
+                [k v])))]
+    (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
 
 (defn json?
   [raw-string]
@@ -12,11 +25,10 @@
   [raw-string keywords?]
   (if (json? raw-string)
     (parse-string raw-string keywords?)
-    (y/parse-string raw-string)))
+    (keywordize-keys (disorder (y/parse-string raw-string false) {}))))
 
 (defn swagger
   [url keywords?]
   (let [raw-response (slurp url)
         parsed (yaml-or-json raw-response keywords?)]
     parsed))
-
